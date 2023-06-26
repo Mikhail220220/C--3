14d04d083a0c582d4f37838013a5377aefb5949e
Mikhail220220 <135328072+Mikhail220220@users.noreply.github.com>
2023-06-26 21:09:01 +0600

    finish

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter text: ");
+
+        string text = Convert.ToString(Console.ReadLine());
+        char[] obrtext = text.ToCharArray();
+        Array.Reverse(obrtext);
+        string finaltext = new string(obrtext);
+
+        if(finaltext == text){
+            Console.WriteLine( "yes");
+        }
+        if (finaltext != text){
+            Console.WriteLine("NO");
+        }
+        //Console.WriteLine();
+
+    }
+}
