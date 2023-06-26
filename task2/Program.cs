29070d096fb5106057c0c3d33cabbc93b013e381
Mikhail220220 <135328072+Mikhail220220@users.noreply.github.com>
2023-06-26 21:12:55 +0600

    finish

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Current(int number)
+{
+int number1 = 1;
+//int result = 1;
+
+
+while (number1 <= number)
+{
+    double result = Math.Pow(number1,3);
+    Console.Write(result + " ," );
+    number1 ++;
+}
+}
+Current (number);
